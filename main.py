28cd70df2580ec75c5930ad23493cae9008ecfde
asdf3118 <asdf2kr@naver.com>
2019-10-08 17:46:25 +0900

    initail ImageClassification-pytorch code.

--- main.py
+++ main.py
@@ -1,58 +1,30 @@
+import os
+import time
+import shutil
 import parser
 import argparse
-
-from tqdm import tqdm
-# import argparser
 
 import torch
 import torch.nn
 import torch.optim
 import torchvision.models as models
+
 import Models.resnet as resnet
 import Models.vggnet as vggnet
 from utils import prepare_dataloaders
+from tqdm import tqdm
 '''
     reference:
-        torchvision
+        pytorch, torchvision
         conda install -c conda-forge torchvision
 '''
-def run_epoch(model, mode, epoch, criterion, optimizer, data_loader, dataset_size):
-    if mode == 'train':
-        model.train()
-    else:
-        model.eval()
-
-    total_loss = 0.
-    total_correct = 0
-    for data, target in tqdm(data_loader, desc='  - (' + mode + ')   ', leave=False):
-        # prepare data
-        data, target = data.cuda(), target.cuda()
-
-        optimizer.zero_grad()
-        # forward
-        output = model(data)
-        loss = criterion(output, target)
-
-        total_loss += loss.item()
-        total_correct += torch.sum(output.data.max(1)[1] == target.data)
-
-        acc1, acc5 = accuracy(output, target, topk=(1,5))
-        if mode == 'train':
-            # backward
-            loss.backward()
-            optimizer.step()
-    print(' - ({}) epoch {} loss {:.4f} acc {:.4f} top1 {:.4f} top5 {:.4f}'.format(mode, epoch, total_loss / dataset_size, total_correct / dataset_size * 100., acc1 / dataset_size * 100. , acc5 / dataset_size * 100.))
-    return acc1 / dataset_size * 100.
-
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
-
 def main():
     ''' Main function '''
     parser = argparse.ArgumentParser(description='Implement image classification on ImageNet datset using pytorch')
-    parser.add_argument('--model', default='resnet50', type=str, help='classification model (resnet(18, 34, 50, 101, 152), vggnet16')
+    parser.add_argument('--arch', default='resnet50', type=str, help='classification model (resnet(18, 34, 50, 101, 152), vggnet16')
+    parser.add_argument('--epoch', default=1, type=int, help='start epoch')
     parser.add_argument('--n_epochs', default=1000, type=int, help='numeber of total epochs to run')
-    parser.add_argument('--batch', default=64, type=int, help='mini batch size (default: 256)')
+    parser.add_argument('--batch', default=96, type=int, help='mini batch size (default: 256)')
     parser.add_argument('--lr', default=0.001, type=float, help='initial learning rate')
     parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
     parser.add_argument('--save_directory', default='trained.chkpt', type=str, help='path to latest checkpoint')
@@ -60,17 +32,21 @@ def main():
     parser.add_argument('--resume', default=False, type=bool, help='resume')
     parser.add_argument('--datasets', default='CIFAR10', type=str, help='classification dataset  (CIFAR10, ImageNet)')
     parser.add_argument('--weight_decay', default=1e-4, type=float, help='weight_decay')
-    parser.add_argument('--save', default='Datas/trained.chkpt', type=str, help='Datas/trained.chkpt')
-    parser.add_argument('--save_multi', default='Datas/trained_multi.chkpt', type=str, help='Datas/trained_multi.chkpt')
+    parser.add_argument('--save', default='trained', type=str, help='trained.chkpt')
+    parser.add_argument('--save_multi', default='trained_multi', type=str, help='trained_multi.chkpt')
+    parser.add_argument('--evaluate', default=False, type=bool, help='evaluate')
     args = parser.parse_args()
-    args.model = args.model.lower()
+    args.arch = args.arch.lower()
     args.datasets = args.datasets.lower()
 
+    if not os.path.isdir('checkpoints'):
+        os.mkdir('checkpoints')
+    # To-do: Write a code relating to seed.
+
     # use gpu or multi-gpu or not.
-    use_gpu = torch.cuda.is_available()
-    if use_gpu :
-        use_multi_gpu = torch.cuda.device_count() > 1
-    print('[Info] use_gpu:{} use_multi_gpu:{}'.format(use_gpu, use_multi_gpu))
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    use_multi_gpu = torch.cuda.device_count() > 1
+    print('[Info] device:{} use_multi_gpu:{}'.format(device, use_multi_gpu))
 
     # load the data.
     print('[Info] Load the data.')
@@ -79,80 +55,157 @@ def main():
     # load the model.
     print('[Info] Load the model.')
 
+
     if args.datasets == 'cifar10':
         num_classes = 10
     elif args.datasets == 'imagenet':
         num_classes = 1000
 
-    if args.model == 'resnet18':
+    if args.arch == 'resnet18':
         model = resnet.resnet18(num_classes=num_classes)
-    elif args.model == 'resnet34':
+    elif args.arch == 'resnet34':
         model = resnet.resnet34(num_classes=num_classes)
-    elif args.model == 'resnet50':
+    elif args.arch == 'resnet50':
         model = resnet.resnet50(num_classes=num_classes)
-    elif args.model == 'resnet101':
+    elif args.arch == 'resnet101':
         model = resnet.resnet101(num_classes=num_classes)
-    elif args.model == 'resnet152':
+    elif args.arch == 'resnet152':
         model = resnet.resnet152(num_classes=num_classes)
-    elif args.model == 'vggnet16':
+    elif args.arch == 'vggnet16':
         model = vggnet.vggnet16(num_classes=num_classes)
 
     # print(count_parameters(model))
     # print('torchvision ', count_parameters(models.resnet50()))
-    epoch_ = 1
-    if use_gpu:
-        model = model.cuda()
-        model = torch.nn.DataParallel(model)
+
+    model = model.to(device)
+    if use_multi_gpu : model = torch.nn.DataParallel(model)
 
     # define loss function.
-    criterion = torch.nn.CrossEntropyLoss().cuda()
+    criterion = torch.nn.CrossEntropyLoss().to(device)
 
     # define optimizer
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
     if args.resume:
         # Load the checkpoint.
-        print('[Info] Resuming from checkpoint.')
+        print('[Info] Loading checkpoint.')
         if torch.cuda.device_count() > 1:
             checkpoint = torch.load(args.save_multi)
         else:
             checkpoint = torch.load(args.save)
 
-        epoch_ = checkpoint['epoch']
-        cnn_ = checkpoint['cnn']
-        model_ = checkpoint['model']
+        arch = checkpoint['arch']
+        args.epoch = checkpoint['epoch']
+        state_dict = checkpoint['state_dict']
+        model.load_state_dict(state_dict)
+        print('[Info] epoch {} arch {}'.format(args.epoch, arch))
 
-        model.load_state_dict(model_)
-        print('[Info] epoch {} base_model {}'.format(epoch_, cnn_))
+    # run evaluate.
+    if args.evaluate:
+        _ = run_epoch(model, 'valid', [args.epoch, args.epoch], criterion, optimizer, valid_loader, valid_size, device)
+        return
 
-    # run epoch.
+    # run train.
+    best_acc1 = 0.
+    for e in range(args.epoch, args.n_epochs + 1):
+        adjust_learning_rate(optimizer, e, args)
 
-    best_acc = 0.
-    for epoch in range(epoch_, args.n_epochs + 1):
-        adjust_learning_rate(optimizer, epoch, args)
-        _ = run_epoch(model, 'train', epoch, criterion, optimizer, train_loader, train_size)
+        # train for one epoch
+        _ = run_epoch(model, 'train', [e, args.n_epochs], criterion, optimizer, train_loader, train_size, device)
+
+        # evaluate on validation set
         with torch.no_grad():
-            acc1 = run_epoch(model, 'valid', epoch, criterion, optimizer, valid_loader, valid_size)
+            acc1 = run_epoch(model, 'valid', [e, args.n_epochs], criterion, optimizer, valid_loader, valid_size, device)
 
         # Save checkpoint.
-        if acc1 > best_acc:
-            best_acc = acc1
-            checkpoint = {
-                'model': model.state_dict(),
-                'epoch': epoch,
-                'cnn': args.model}
-            torch.save(checkpoint, args.save)
+        is_best = acc1 > best_acc1
+        best_acc1 = max(acc1, best_acc1)
+        save_checkpoint({
+            'epoch': e,
+            'arch': args.arch,
+            'state_dict': model.state_dict(),
+            'best_acc1': best_acc1,
+            'optimizer': optimizer.state_dict(),
+        }, is_best, args.save)
 
-            if torch.cuda.device_count() > 1:
-                checkpoint_module = {
-                    'model': model.module.state_dict(),
-                    'epoch': epoch,
-                    'cnn': args.model}
+        save_checkpoint({
+            'epoch': e,
+            'arch': args.arch,
+            'state_dict': model.module.state_dict(),
+            'best_acc1': best_acc1,
+            'optimizer': optimizer.state_dict(),
+        }, is_best & use_multi_gpu, args_save_multi)
 
-                torch.save(checkpoint_module, args.save_multi)
-            print(' - [Info] The checkpoint file has been updated.')
+        print('[Info] acc1 {} best@acc1 {}'.format(acc1, best_acc1))
 
-        print('[Info] acc1 {} best {}'.format(acc1, best_acc))
+def run_epoch(model, mode, epoch, criterion, optimizer, data_loader, dataset_size, device):
+    if mode == 'train':
+        model.train()
+    else:
+        model.eval()
+
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    start = time.time()
+    tq = tqdm(data_loader, desc='  - (' + mode + ')   ', leave=False)
+    for data, target in tq:
+        # prepare data
+        data, target = data.to(device), target.to(device)
+
+        # forward
+        output = model(data)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1,5))
+        losses.update(loss.item(), data.size(0))
+        top1.update(prec1[0], data.size(0))
+        top5.update(prec5[0], data.size(0))
+
+        if mode == 'train':
+            # compte gradient and do SGD step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        tq.set_description(' - ({}) [ epoch: {}/{} loss: {:.3f}/{:.3f} ] '.format(mode, epoch[0], epoch[1], losses.val, losses.avg))
+        #tqdm.write
+    tqdm.write(' - ({})  [ epoch: {}\ttop@1: {:.3f}\ttop@5: {:.3f}\ttime: {:.3f}]'.format(mode, epoch, top1.avg, top5.avg, (time.time() - start)/60.))
+
+
+    return top1.avg
+
+def save_checkpoint(state, is_best, prefix):
+    filename='checkpoints/{}_checkpoint.chkpt'.format(prefix)
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'checkpoints/{}_best.chkpt'.format(prefix))
+    print(' - [Info] The checkpoint file has been updated.')
+
+class AverageMeter(object):
+    '''Computes and stores the average and current value'''
+    def __init__(self):
+        self.reset()
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def adjust_learning_rate(optimizer, epoch, args):
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
 
 def accuracy(output, target, topk=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k"""
@@ -170,13 +223,8 @@ def accuracy(output, target, topk=(1,)):
         res = []
         for k in topk:
             correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k)
+            res.append(correct_k.mul_(100.0 / bsz))
         return res
-
-def adjust_learning_rate(optimizer, epoch, args):
-    lr = args.lr * (0.1 ** (epoch // 30))
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
 
 if __name__ == '__main__':
 	main()
